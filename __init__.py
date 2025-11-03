2ee74bdd8e985989ce3f992b3dcc885a0f9aa5ff
THEman6989 <129108560+THEman6989@users.noreply.github.com>
2025-11-03 20:52:35 +0100

    Refine standalone automatic pose editor

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .pose_data_editor_alone_automatic import PoseDataEditorAloneAutomatic
 
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    "PoseDataEditorAloneAutomatic",
+]
